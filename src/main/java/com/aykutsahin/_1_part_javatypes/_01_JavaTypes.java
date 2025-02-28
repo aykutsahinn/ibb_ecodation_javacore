605360744dea87eb24eb47f81faefdf22b2c2e8c
aykutsahinn <sahinaykut97@gmail.com>
2025-02-28 23:29:25 +0300

    Java Class Created

--- src/main/java/com/aykutsahin/_1_part_javatypes/_01_JavaTypes.java
+++ src/main/java/com/aykutsahin/_1_part_javatypes/_01_JavaTypes.java
@@ -1,4 +1,12 @@
 package com.aykutsahin._1_part_javatypes;
 
+/*
+Multiple
+ */
+
 public class _01_JavaTypes {
-}
+    //psvm => tab
+    public static void main(String[] args) {
+        System.out.println("Merhabalar");
+    } //end psvm
+} // end class
