01f8d700b501641a5d87b3b5bf78fdbcc368805a
aykutsahinn <sahinaykut97@gmail.com>
2025-03-01 00:19:05 +0300

    Java Class Created

---
+++ src/main/java/com/aykutsahin/_1_part_javatypes/_02_Variables.java
@@ -0,0 +1,19 @@
+package com.aykutsahin._1_part_javatypes;
+
+public class _02_Variables {
+    public static void main(String[] args) {
+
+        int schoolNumber = 25;
+        System.out.println(schoolNumber);
+
+        schoolNumber = 44;
+        System.out.println(schoolNumber);
+
+        // değişken isimleri _ veya $ ile başlayabilir
+        int _schoolNumber = 85;
+        System.out.println(_schoolNumber);
+
+        int $schoolNumber = 75;
+        System.out.println($schoolNumber);
+    }
+}
