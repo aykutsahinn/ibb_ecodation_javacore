b712ce511a58ab631d497950e48450c8f59661d1
aykutsahinn <sahinaykut97@gmail.com>
2025-03-01 14:18:15 +0300

    Java Class Created

---
+++ src/main/java/com/aykutsahin/_2_week/_14_1_Exception.java
@@ -0,0 +1,44 @@
+package com.aykutsahin._2_week;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+     Exception in thread "main" java.lang.NullPointerException: Neden boş verdiniz
+	at com.aykutsahin._2_week._14_1_Exception.main(_14_1_Exception.java:42)
+       */
+
+public class _14_1_Exception {
+    public static void calcula() throws ArithmeticException, IOException, NullPointerException {
+        Scanner scanner = new Scanner(System.in);
+        int number1, number2;
+        System.out.println("Number 1 giriniz");
+        number1 = scanner.nextInt();
+
+        System.out.println("Number 2 giriniz");
+        number2 = scanner.nextInt();
+        try {
+            int result = number1 / number2;
+            System.out.println(result);
+
+        } catch (ArithmeticException ai) {
+            ai.printStackTrace();
+            //ai.getMessage();
+        } catch (Exception e) {
+            e.printStackTrace();
+            //ai.getMessage();
+        } finally {
+            System.out.println("İstisna olsada olmazsada mutlaka yapılacak iş");
+            scanner.close();
+        }
+        System.out.println("Son 10.000 satır");
+    }
+
+    public static void main(String[] args) throws IOException {
+        calcula();
+        System.out.println("################################");
+
+        // Bilerek istisna gönder
+        throw new NullPointerException("Neden boş verdiniz");
+    }
+}
